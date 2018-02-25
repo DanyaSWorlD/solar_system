e494c8d4201ecc4193b5989e7f6a7a34979fb246
Daniil Mirenskiy <dmirensky@sang.ru>
2018-02-25 14:07:45 +0300

    4 errors

--- solar_system/solar_system/Source.cpp
+++ solar_system/solar_system/Source.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
+
+class planet;
+class carlic_planet;
+class comet;
+class asteroid;
 
 class Cosmic_body
 {
@@ -10,8 +16,61 @@ class Cosmic_body
 	int _days;
 	int _temperature;
 	int _radius;
+	bool _abort;
+
+	static bool checkExit(string s)
+	{
+		if (s == "/stop")
+			return true;
+		return false;
+	}
+
+	void putValByIndex(string val, int index)
+	{
+		void(Cosmic_body::*funcs[])(int) = {
+			&Cosmic_body::SetDistanceFromSun,&Cosmic_body::SetSize,&Cosmic_body::SetRotTime,&Cosmic_body::SetTemperature,&Cosmic_body::SetRadius
+		};
+
+		//typedef void(Cosmic_body::*fun)(int);
+		//static fun methods[5] = { &Cosmic_body::SetDistanceFromSun,&Cosmic_body::SetSize,&Cosmic_body::SetRotTime,&Cosmic_body::SetTemperature,&Cosmic_body::SetRadius };
+
+		if (index == 0)
+		{
+			SetName(val);
+			return;
+		}
+
+		index--;
+		(this->*funcs[index])(stoi(val));
+	}
+
+	string msgs[6] = {
+		"Введите название\n",
+		"Введите расстояние от солнца\n",
+		"Введите размер\n",
+		"Введите количество суток, за которое тело обращается вокруг солнца\n",
+		"Введите температуру\n",
+		"Введите радиус\n"
+	};
+
+	void ReadFromFile(FILE* f)
+	{
+		fscanf(f, "%s", _name);
+		fscanf(f, "%i", _distance);
+		fscanf(f, "%i", _size);
+		fscanf(f, "%i", _days);
+		fscanf(f, "%i", _temperature);
+		fscanf(f, "%i", _radius);
+	}
+
 public:
 	Cosmic_body();
+	Cosmic_body(bool fromConsole)
+	{
+		if (fromConsole)
+			ReadFromConsole();
+	}
+
 	virtual string GetClassName();
 	string GetName()
 	{
@@ -62,9 +121,13 @@ public:
 		_radius = radius;
 	}
 
+	bool aborted()
+	{
+		return _abort;
+	}
 	void SaveToFile(string FileName)
 	{
-		FILE* f = fopen(FileName.c_str(), "w");
+		FILE* f = fopen(FileName.c_str(), "a");
 		string s;
 		s.append(GetClassName()).append(" ").append(GetName()).append("\n");
 		s.append(std::to_string(GetDistanceFormSun())).append("\n");
@@ -87,48 +150,102 @@ public:
 		cout << s;
 	}
 
-	void ReadFromFile(string FileName)
+	void ReadFromConsole()
 	{
-		FILE * f = fopen(FileName.c_str(), "r");
-		string s;
-		fscanf(f, "%s", s);
-		fscanf(f, "%s", _name);
-		fscanf(f, "%i", _distance);
-		fscanf(f, "%i", _size);
-		fscanf(f, "%i", _days);
-		fscanf(f, "%i", _temperature);
-		fscanf(f, "%i", _radius);
+		string inp;
+		cout << "Заполните все параметры космического тела. Для отмены введите /stop";
+		for (int i = 0; i < 6; i++)
+		{
+			cout << msgs[i];
+			cin >> inp;
+			if (checkExit(inp))
+			{
+				_abort = true;
+				break;
+			}
+			putValByIndex(inp, i);
+		}
+		cout << "\n";
 	}
+
+	static Cosmic_body* ReadFromFile(string file, int * size)
+	{
+		*size = 0;
+		Cosmic_body *ff = (Cosmic_body*)malloc(sizeof(Cosmic_body));
+		FILE* f = fopen(file.c_str(), "r");
+		while (1)
+		{
+			if (!feof(f))
+			{
+				ff = (Cosmic_body*)realloc(ff,sizeof(Cosmic_body)*((*size) + 1));
+				Cosmic_body* b;
+				string type;
+				cin >> type;
+				if (type == "planet")
+					b = new planet();
+
+				if (type == "carlic_planet")
+					b = new carlic_planet();
+
+				if (type == "asteroid")
+					b = new asteroid();
+
+				if (type == "comet")
+					b = new comet();
+				
+				b->ReadFromFile(f);
+				ff[*size] = (*b);
+			}
+			else break;
+		}
+		return ff;
+	};
 
 	virtual ~Cosmic_body();
 };
 
-class planet: public Cosmic_body
+class planet : public Cosmic_body
 {
+public:
+	planet() : Cosmic_body() {}
+	planet(bool fromConsole) : Cosmic_body(fromConsole) {	}
+
 	virtual string GetClassName()
 	{
 		return "planet";
 	}
 };
 
-class carlic_planet: public Cosmic_body
+class carlic_planet : public Cosmic_body
 {
+public:
+	carlic_planet() {}
+	carlic_planet(bool fromConsole) : Cosmic_body(fromConsole) {	}
+
 	virtual string GetClassName()
 	{
 		return "carlic_planet";
 	}
 };
 
-class asteroid: public Cosmic_body
+class asteroid : public Cosmic_body
 {
+public:
+	asteroid() {}
+	asteroid(bool fromConsole) : Cosmic_body(fromConsole) {	}
+
 	virtual string GetClassName()
 	{
 		return "asteroid";
 	}
 };
 
-class comet: public Cosmic_body
+class comet : public Cosmic_body
 {
+public:
+	comet() {}
+	comet(bool fromConsole) : Cosmic_body(fromConsole) {	}
+
 	virtual string GetClassName()
 	{
 		return "comet";
@@ -142,12 +259,12 @@ public:
 	virtual ~Creator();
 };
 
-class PlanetCreator: public Creator
+class PlanetCreator : public Creator
 {
 public:
 	virtual  Cosmic_body* FactoryMethod()
 	{
-		return new planet();
+		return new planet(true);
 	};
 };
 
@@ -156,7 +273,7 @@ class CarlicPlanetCreator : public Creator
 public:
 	virtual  Cosmic_body* FactoryMethod()
 	{
-		return new carlic_planet();
+		return new carlic_planet(true);
 	};
 };
 
@@ -165,7 +282,7 @@ class AsteroidCreator : public Creator
 public:
 	virtual  Cosmic_body* FactoryMethod()
 	{
-		return new asteroid();
+		return new asteroid(true);
 	};
 };
 
@@ -174,10 +291,79 @@ class CometCreator : public Creator
 public:
 	virtual  Cosmic_body* FactoryMethod()
 	{
-		return new comet();
+		return new comet(true);
 	};
 };
 
-void main (void){
+string* split(string str, char splitSymbol = ' ')
+{
+	vector<string> mass = vector<string>();
+	string tmp = "";
+	for each(char c in str)
+	{
+		if (c == splitSymbol)
+		{
+			if (tmp.length() > 0)
+			{
+				mass.push_back(tmp);
+				tmp = "";
+			}
+		}
+		else
+			tmp.push_back(c);
+	}
+	string* ret = (string*)malloc(sizeof(string)*mass.size());
+	for (int i = 0; i < mass.size(); i++)
+		ret[i] = mass.at(i);
+
+	return ret;
+};
+
+string mainfile = "system.txt";
+vector<Cosmic_body> ssystem = vector<Cosmic_body>();
+
+void main(void) {
+	static const size_t count = 4;
+	Creator*creators[] = {
+		&PlanetCreator(),&CarlicPlanetCreator(),&AsteroidCreator(),&CometCreator() };
+	string command;
+	while (1) {
+		cout << "Введите команду. Для справки введите help\n";
+		cin >> command;
+		string* comArr = split(command);
+		if (comArr[0] == "save") {
+			FILE * f = fopen(mainfile.c_str(), "w");
+			for each (Cosmic_body b in ssystem)
+				b.SaveToFile(mainfile);
+		}
+
+		if (comArr[0] == "input") {
+			if (comArr[1] == "planet")
+				ssystem.push_back(creators[0]->FactoryMethod());
+			if (comArr[1] == "carlic")
+				if (comArr[2] == "planet")
+					ssystem.push_back(creators[1]->FactoryMethod());
+			if (comArr[1] == "asteroid")
+				ssystem.push_back(creators[2]->FactoryMethod());
+			if (comArr[1] == "comet")
+				ssystem.push_back(creators[3]->FactoryMethod());
+		}
+
+		if (comArr[0] == "help")
+		{
+
+		}
+
+		if (comArr[0] == "file")
+			if (comArr[1] == "open") {
+				int count = 0;
+				Cosmic_body* bodyes = Cosmic_body::ReadFromFile(mainfile, &count);
+				for (int i = 0; i < count; i++)
+					ssystem.push_back(bodyes[i]);
+			}
+
+		if (comArr[0] == "exit")
+			break;
+	}
 
 }
